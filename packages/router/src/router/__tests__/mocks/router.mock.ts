e6a569d71a93c2331c112903dfd2da0e05547a97
Kadir Boylu <kadir.boylu@outlook.com>
2023-04-27 14:55:47 +0300

    feat(router): add unit tests

---
+++ packages/router/src/router/__tests__/mocks/router.mock.ts
@@ -0,0 +1,63 @@
+import { Route } from "@/types";
+
+interface User {
+    id: number;
+    name: string;
+}
+
+const users: User[] = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+];
+
+const routes: Route[] = [
+    {
+        name: "home",
+        path: "/",
+        component: () => "<h1>Home</h1>",
+    },
+    {
+        name: "about",
+        path: "/about",
+        component: () => "<h1>About</h1>",
+    },
+    {
+        name: "users",
+        path: "/users",
+        component: () => {
+            return `
+              <h1>Users</h1>
+              <ul>
+                  ${users
+                      .map((user: any) => {
+                          return `<li><a href="/users/${user.id}" class="router-link">${user.name}</a></li>`;
+                      })
+                      .join("")}
+              </ul>
+          `;
+        },
+    },
+    {
+        name: "user",
+        path: "/users/:id",
+        component: (router) => {
+            const user = users.find(
+                (user: User) => user.id === Number(router.params.id)
+            );
+
+            if (!user) return "<h1>User not found</h1>";
+
+            return `
+              <h1>User with id ${user.id}</h1>
+              <p>${user.name}</p>
+          `;
+        },
+    },
+    {
+        name: "not-found",
+        path: "*",
+        component: () => "<h1>Not Found</h1>",
+    },
+];
+
+export { routes };
--- packages/router/src/router/create-router.ts
+++ packages/router/src/router/create-router.ts
@@ -1,15 +1,12 @@
 import { Router } from "./router";
-import type { Route } from "../types";
+import type { Route } from "@/types";
 
 export const createRouter = (routes: Route[]) => {
     const router = new Router(routes);
 
-    router
-        .render()
-        .then(() => {
-            return router;
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+    router.render().catch((err) => {
+        console.error(err);
+    });
+
+    return router;
 };
